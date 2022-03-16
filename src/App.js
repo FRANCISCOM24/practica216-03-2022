63b044e246e1bc1ad41e909c0730815ea0a08b9e
Centro de Computo <ccomputo@itvy.edu.mx>
2022-03-16 10:20:08 -0700

    practica 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function Componente(props) {
+  return (
+    <h4>Datos: {props.nombre} {props.apellido}</h4>
+  );
+}
+function Viñetas(props) {
+  return (
+    <div>
+      <ul>
+        <li>
+          {props.valor1}
+        </li>
+        <li>
+          {props.valor2}
+        </li>
+        <li>
+          {props.valor3}
+        </li>
+      </ul>
+    </div>
+
+  );
+
+}
+function Viñetas_html() {
+
+  return (
+
+    <div>
+      viñetas html
+      <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+      </ul>
+    </div>
+  );
+
+}
+const valores = ["a", "b", "c"];
+
+const llenar = valores.map((valores) =>
+  <li>{valores}</li>
+
+);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Viñetas react
+        <Viñetas
+          valor1="a"
+          valor2="b"
+          valor3="c"></Viñetas>
+        <Viñetas_html></Viñetas_html>
+        <Componente
+          nombre="Jesus Francisco"
+          apellido="Morales Montoya"></Componente>
+        <ul>{llenar}</ul>
       </header>
     </div>
   );
